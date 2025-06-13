63531d89c2582672fe406ad6a4f39d16e09445ed
DmitryDubovikov <dmitry.dubovikov@gmail.com>
2025-06-13 20:22:14 -0300

    Add Flask-CORS, add fetch to html

--- src/api/app.py
+++ src/api/app.py
@@ -1,10 +1,12 @@
 import logging
 from flask import Flask
+from flask_cors import CORS
 from src.infrastructure.chat_client import ChatService
 from src.api.controllers import register_routes
 
 def create_app():
     app = Flask(__name__)
+    CORS(app)  # Enable CORS for all routes
 
     log = logging.getLogger('werkzeug')
     log.setLevel(logging.WARNING)
